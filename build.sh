b8d6a5677a671667fb2103221d2d8dcae00ab57e
BigBrother1984 <stevewatersy@gmail.com>
2013-11-07 12:57:52 +0100

    4.4 Build script bring up.
    From @eugenesan rework:
    - Remove useless not-pa cases code.
    - Update vendor index.
    - Fixed version grepping.
    - Remove old tools actions.
    - AOSPAed.
    - Remove CM case (extra-packages).
    - Remove  ccache code.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# get current path
-reldir=`dirname $0`
-cd $reldir
-DIR=`pwd`
+# Version 2.0.4, Adapted for AOSPA.
 
-# Colorize and add text parameters
+# We don't allow scrollback buffer
+echo -e '\0033\0143'
+clear
+
+# Get current path
+DIR="$(cd `dirname $0`; pwd)"
+OUT="$(readlink $DIR/out)"
+[ -z "${OUT}" ] && OUT="${DIR}/out"
+
+# Prepare output customization commands
 red=$(tput setaf 1)             #  red
 grn=$(tput setaf 2)             #  green
+blu=$(tput setaf 4)             #  blue
 cya=$(tput setaf 6)             #  cyan
 txtbld=$(tput bold)             # Bold
 bldred=${txtbld}$(tput setaf 1) #  red
@@ -16,81 +23,99 @@ bldblu=${txtbld}$(tput setaf 4) #  blue
 bldcya=${txtbld}$(tput setaf 6) #  cyan
 txtrst=$(tput sgr0)             # Reset
 
-THREADS="16"
+# Local defaults, can be overriden by environment
+: ${PREFS_FROM_SOURCE:="false"}
+: ${THREADS:="$(cat /proc/cpuinfo | grep "^processor" | wc -l)"}
+
+# If there is more than one jdk installed, use latest 6.x
+if [ "`update-alternatives --list javac | wc -l`" -gt 1 ]; then
+        JDK6=$(dirname `update-alternatives --list javac | grep "\-6\-"` | tail -n1)
+        JRE6=$(dirname ${JDK6}/../jre/bin/java)
+        export PATH=${JDK6}:${JRE6}:$PATH
+fi
+JVER=$(javac -version  2>&1 | head -n1 | cut -f2 -d' ')
+
+# Import command line parameters
 DEVICE="$1"
 EXTRAS="$2"
 
-# get current version
-MAJOR=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAJOR = *' | sed  's/ROM_VERSION_MAJOR = //g')
-MINOR=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MINOR = *' | sed  's/ROM_VERSION_MINOR = //g')
-MAINTENANCE=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAINTENANCE = *' | sed  's/ROM_VERSION_MAINTENANCE = //g')
+# Get build version
+MAJOR=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAJOR := *' | sed  's/ROM_VERSION_MAJOR := //g')
+MINOR=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MINOR := *' | sed  's/ROM_VERSION_MINOR := //g')
+MAINTENANCE=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAINTENANCE := *' | sed  's/ROM_VERSION_MAINTENANCE := //g')
+
 VERSION=$MAJOR.$MINOR$MAINTENANCE
 
-# if we have not extras, reduce parameter index by 1
-if [ "$EXTRAS" == "true" ] || [ "$EXTRAS" == "false" ]
-then
-   SYNC="$2"
-   UPLOAD="$3"
+# If there is no extra parameter, reduce parameters index by 1
+if [ "$EXTRAS" == "true" ] || [ "$EXTRAS" == "false" ]; then
+        SYNC="$2"
+        UPLOAD="$3"
 else
-   SYNC="$3"
-   UPLOAD="$4"
+        SYNC="$3"
+        UPLOAD="$4"
 fi
 
-# get time of startup
+# Get start time
 res1=$(date +%s.%N)
 
-# we don't allow scrollback buffer
-echo -e '\0033\0143'
-clear
+echo -e "${cya}Building ${bldcya}AOSPA $VERSION for $DEVICE ${txtrst}";
+echo -e "${bldgrn}Start time: $(date) ${txtrst}"
 
-echo -e "${cya}Building ${bldcya}ParanoidAndroid v$VERSION ${txtrst}";
-
-#echo -e "${cya}"
-#./vendor/pa/tools/getdevicetree.py $DEVICE
-#echo -e "${txtrst}"
-
-# decide what command to execute
+# Decide what command to execute
 case "$EXTRAS" in
-   threads)
-       echo -e "${bldblu}Please write desired threads followed by [ENTER] ${txtrst}"
-       read threads
-       THREADS=$threads;;
-   clean)
-       echo -e ""
-       echo -e "${bldblu}Cleaning intermediates and output files ${txtrst}"
-       make clean > /dev/null;;
+        threads)
+                echo -e "${bldblu}Please enter desired building/syncing threads number followed by [ENTER]${txtrst}"
+                read threads
+                THREADS=$threads
+        ;;
+        clean|cclean)
+                echo -e "${bldblu}Cleaning intermediates and output files${txtrst}"
+                export CLEAN_BUILD="true"
+                [ -d "${DIR}/out" ] && rm -Rf ${DIR}/out/*
+        ;;
 esac
 
-# sync with latest sources
 echo -e ""
-if [ "$SYNC" == "true" ]
-then
-   echo -e "${bldblu}Fetching latest sources ${txtrst}"
-   repo sync -j"$THREADS"
-   echo -e ""
+
+# Fetch latest sources
+if [ "$SYNC" == "true" ]; then
+        echo -e ""
+        echo -e "${bldblu}Fetching latest sources${txtrst}"
+        repo sync -j"$THREADS"
+        echo -e ""
 fi
 
-# java setup
-# echo -e "${bldblu}Setting up java ${txtrst}"
-# . vendor/pa/tools/setupjava.sh
-# echo -e ""
+if [ ! -r "${DIR}/out/versions_checked.mk" ] && [ -n "$(java -version 2>&1 | grep -i openjdk)" ]; then
+        echo -e "${bldcya}Your java version still not checked and is candidate to fail, masquerading.${txtrst}"
+        JAVA_VERSION="java_version=${JVER}"
+fi
 
-# setup environment
-echo -e "${bldblu}Setting up environment ${txtrst}"
-. build/envsetup.sh
+if [ -n "${INTERACTIVE}" ]; then
+        echo -e "${bldblu}Dropping to interactive shell${txtrst}"
+        echo -en "${bldblu}Remeber to lunch you device:"
+        if [ "${VENDOR}" == "pa" ]; then
+                echo -e "[${bldgrn}lunch pa_$DEVICE-userdebug${bldblu}]${txtrst}"
+        else
+                echo -e "[${bldgrn}lunch full_$DEVICE-userdebug${bldblu}]${txtrst}"
+        fi
+        bash --init-file build/envsetup.sh -i
+else
+        # Setup environment
+        echo -e ""
+        echo -e "${bldblu}Setting up environment${txtrst}"
+        . build/envsetup.sh
+        echo -e ""
 
-# lunch device
+        # lunch/brunch device
+        echo -e "${bldblu}Lunching device [$DEVICE] ${cya}(Includes dependencies sync)${txtrst}"
+        export PREFS_FROM_SOURCE
+        lunch "pa_$DEVICE-userdebug";
+
+        echo -e "${bldblu}Starting compilation${txtrst}"
+        mka bacon
+fi
 echo -e ""
-echo -e "${bldblu}Lunching device ${txtrst}"
-lunch "pa_$DEVICE-userdebug";
 
-echo -e ""
-echo -e "${bldblu}Starting compilation ${txtrst}"
-
-# start compilation
-mka bacon
-echo -e ""
-
-# finished? get elapsed time
+# Get elapsed time
 res2=$(date +%s.%N)
-echo "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
+echo -e "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds)${txtrst}"
